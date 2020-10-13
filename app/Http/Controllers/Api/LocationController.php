7381377f853f12760ff5a034cdea247df8664065
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-13 15:14:37 -0400

    cambios

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -82,7 +82,7 @@ class LocationController extends BaseApiController
         e3oKKv8rd80:APA91bEL2wQVFc8reZNqNFgtmhimeC7VMzHbPf4i6Nqy5O0ha1aoHoijV-VRYsA12Bof0X74t5diz8m9biXbhyOSRUI0nke2XqYS5Uto4OaUVx9RAcJbYwZ-IhKyIbkTytTw_278bcMX
         */
         fcm()
-            ->to(["e3oKKv8rd80:APA91bEL2wQVFc8reZNqNFgtmhimeC7VMzHbPf4i6Nqy5O0ha1aoHoijV-VRYsA12Bof0X74t5diz8m9biXbhyOSRUI0nke2XqYS5Uto4OaUVx9RAcJbYwZ-IhKyIbkTytTw_278bcMX"])
+            ->to([$devicesArray])
             ->notification([
               'title' => "📍Atención Comunidad Traya",
               'body' => $request->description,
