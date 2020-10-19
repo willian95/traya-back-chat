64600259f7d701b56ab0d351b31873215fbd76ce
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-19 07:59:38 -0400

    cambios

--- app/Http/Controllers/ClaimController.php
+++ app/Http/Controllers/ClaimController.php
@@ -34,7 +34,7 @@ class ClaimController extends Controller
 
                 $data = ["description" => $claim->description, "images" => ClaimImage::where("claim_id", $claim->id)->get()];
                 $to_name = "Admin";
-                $to_email = "rodriguezwillian95@gmail.com";
+                $to_email = "adm.traya@gmail.com";
 
                 \Mail::send("emails.claim", $data, function($message) use ($to_name, $to_email) {
 
